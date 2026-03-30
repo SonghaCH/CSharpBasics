070d2c47deebcf3074c192de922a6514b7b6fa05
SonghaCH <cwithus0002@gmail.com>
2026-03-30 18:03:56 +0900

    [Init] C# 입문, 두 정수로 사칙연산 하는 계산기 만들기 20260330
    C# 을 입문하게 되어 기본적인 연산자와 형변형을 이용하여 입출력 장치를 이용하였으며, 아직은 부족하지만 기본적인 정수로 받는 사칙연산 계산기를 만들었다.
    추후 예외 처리 등을 신경쓰어 범위 내에서 구동 가능한 계산하고, 기술을 더욱 습득해 정수 뿐만 아니라 여러수도 입력 받으며 계산하는 계산기를 만들 수 있을거 같다는 생각이 들었다.
    AI 없이 만든것이 뿌듯했으며, parse 라는 형변자의 존재 유무도 알게 되었다.

---
+++ CSBasics/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 0f;
+            float b = 0f;
+
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("간단한 정수 계산기 만들기!");
+            Console.WriteLine("연산 형식은 a ㅁ b 형태로 ");
+            Console.WriteLine("a의 숫자와 b 숫자를 대입한 후");
+            Console.WriteLine("사칙연산 기호를 선택하여 계산합니다");
+            Console.WriteLine("*주의* 예외처리 장치 구현이 되지않아 올바른 값을 입력하시길 바랍니다!");
+            Console.WriteLine("---------------------------------------");
+
+
+            Console.WriteLine("a 에 들어갈 수를 입력 하시오.");
+            a = float.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("b 에 들어갈 수를 입력 하시오.");
+            b= float.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("사칙 연산할 연산 기호를 입력하시오, ");
+            Console.WriteLine("+ - * / ");
+
+
+
+            while (true)
+            {
+
+                string op = Console.ReadLine();
+                
+
+                if (op == "+")
+                {
+                    Console.WriteLine(a + b);
+                }
+                else if (op == "-")
+                {
+                    Console.WriteLine(a - b);
+                }
+                else if (op == "*")
+                {
+                    Console.WriteLine(a * b);
+                }
+                else if (op == "/")
+                {
+                    Console.WriteLine(a / b);
+                }
+                else if (op == "0")
+                {
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("다른 기호를 입력해주세요");
+                }
+
+                Console.WriteLine("\n\n다른 사칙 연산을 하려면 다른 기호를 입력하세요.\n\n");
+                Console.WriteLine("\n\n{계산을 그만하시려면 0번을 눌러주세요}\n\n");
+            }
+
+
+        }
+    }
+}
