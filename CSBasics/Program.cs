5010b4c37c188fb218d0e15b65d83e15cee4a4f3
SonghaCH <cwithus0002@gmail.com>
2026-04-02 00:52:00 +0900

    [Feat] C++ '행운의 잭팟' 게임 C#으로 변환하기 20260401
    기존 C++ 에서 만들어본 행운의 잭팟 게임을 C# 으로 똑같이 구현 하였을때, 무엇이 다르고 구조에서는 어떻게 다른지 비교하며 변환하였는데, 사실 기본 로직의 형태는 거의 같았으며, 입출력 부분과 헤더 부분 외에는 거의 유사하였다.
    확실히 업그레이드 된듯한 느낌이었으며, 비교적 편리한 언어라는걸 몸소 체험하였다.

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -1,84 +1,92 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Tracing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CSBasics
+namespace LuckyDrawGame
 {
-    public enum MachineState : byte
+    class Program
     {
-        Off,
-        Running,
-        Broken
-    }
-    public class Machine
-    {
-        private string m_name;
-        private float m_weight;
-        private int m_price;
-        private MachineState m_state = MachineState.Off;
-
-        public Machine(string name, float weight, int price, MachineState state)
-        {
-            this.m_name = name;
-            this.m_weight = weight;
-            this.m_price = price;
-            this.m_state = state;
-            Console.WriteLine($"새로운 {m_name}를 구매하였습니다.");
-        }
-        public string GetMachineName()
-        {
-            return m_name;
-        }
-
-        public MachineState GetState()
-        {
-            return m_state;
-        }
-
-        public void SetState(MachineState state)
-        {
-            m_state = state;
-            Console.WriteLine($"기계의 상태가 {m_state}로 변경되었습니다.");
-        }
-    }
-
-    public class Mechanic
-    {
-        private string m_name;
-
-        public Mechanic(string name)
-        {
-            m_name = name;
-        }
-        public void Repair(Machine targetMachine)
-        {
-            Console.WriteLine($"[정비중]  {m_name} 정비사가 {targetMachine.GetMachineName()}의 점검을 시작합니다.");
-            if (targetMachine.GetState() == MachineState.Broken)
-            {
-                targetMachine.SetState(MachineState.Off);
-                Console.WriteLine($"-> {targetMachine.GetMachineName()} 수리 완료! (현재 상태: Off)");
-            }
-            else
-            {
-                Console.WriteLine("기계 이상없습니다.");
-            }
-        }
-    }
-    internal class Program
-    {
-
-
-
-
         static void Main(string[] args)
         {
-            Machine myMouse = new Machine("고사양 게이밍 마우스", 0.6f, 300000, MachineState.Off);
-            Mechanic myMechanic = new Mechanic("차송하");
-            myMechanic.Repair(myMouse);
+            
+            Random rand = new Random(); //C# 스타일 랜덤함수 작성
+            int JackPot = rand.Next(1, 21); // 1부터 20까지의 랜덤수 생성
 
+            int CountCoin = 10;
+            int ChoiceNumber;
+            int EnterButton;
+
+            Console.WriteLine("*반갑습니다. 행운의 뽑기 시간입니다.*");
+            Console.WriteLine("\n코인은 총 10개이며 10번안에 맞추지 못할시 게임오버 됩니다.");
+            Console.WriteLine("\n1부터 20까지 숫자이며 정답은 단 1개입니다.");
+
+            for (; ; )  // 0또는 1번을 눌렀을때, 다시 시도하는 반복문
+            {
+                Console.WriteLine("<진행하려면 (1)번을, 종료 하시려면 (0)번을 눌러주세요.>\n");
+
+                // 입력받은 문자열을 정수로 변환
+                if (int.TryParse(Console.ReadLine(), out EnterButton))
+                {
+                    if (EnterButton == 1)
+                    {
+                        Console.WriteLine("게임을 시작하겠습니다!");
+                        break;
+                    }
+                    else if (EnterButton == 0)
+                    {
+                        Console.WriteLine("종료 버튼을 누르셨습니다! \n게임이 종료 됩니다.");
+                        return; // 프로그램 종료
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n잘못 입력하셨습니다. 1번 혹은 0번을 입력해주세요");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\n숫자만 입력해주세요.");
+                }
+            }
+
+            Console.WriteLine("1부터 20까지 입력하여 당첨을 찾아십시오.");
+
+            for (int i = 1; i <= CountCoin; i++)
+            {
+                for (; ; ) // 1부터 20사이만 입력하도록 반복
+                {
+                    if (int.TryParse(Console.ReadLine(), out ChoiceNumber))
+                    {
+                        if (ChoiceNumber >= 1 && ChoiceNumber <= 20)  // 정상 입력시 진행
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\n1부터 20사이만 입력해주세요.(코인은 차감되지않습니다.)");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n숫자만 입력해주세요.");
+                    }
+                }
+
+                if (ChoiceNumber == JackPot)
+                {
+                    Console.WriteLine("\n7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7\n잭팟! 정답입니다! 상품은 주강사님의 *격려* 입니다\n축하드립니다!");
+                    Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+                    return; // 프로그램 종료
+                }
+                else
+                {
+                    Console.WriteLine("\n아쉽게도 정답이 아닙니다");
+                    Console.WriteLine("남은 코인:  " + (CountCoin - i) + " 개 남았습니다. 상품이 매우 값진 보상이니 화이팅!");
+
+                    if (i == CountCoin)
+                    {
+                        Console.WriteLine("\n\n\n\n\n모든 코인을 소진하셨습니다. \nG  a   m  e       O  v  e   r");
+                        Console.WriteLine("\n\n\n\n<<아쉽게도 정답은 [" + JackPot + "] 이었습니다.>>");
+                    }
+                }
+            }
         }
     }
 }
