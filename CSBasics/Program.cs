646f34899ec227631bf61cea6d9ece368795ba57
SonghaCH <cwithus0002@gmail.com>
2026-04-01 12:59:17 +0900

    [class] C#의 클래스 구조와 이해를 통한 객체 만들기 20260331
    C++과는 다른 C# 객체 이해와 구조를 직접 사용하여 클래스를 생성하고,
    객체를 만드는 순서와 그에 맞는 설계를 메모하며 글 -> 코드로 옮기는 연습을 해보았다. AI를 참고하지 않다보니 다소 퀄리티가 떨어지는 모습을 보니 열심히 하고
    추후 업데이트 할 수 있도록 노력해야겠다고 생각했다.

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -1,77 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSBasics
 {
+    public enum MachineState : byte
+    {
+        Off,
+        Running,
+        Broken
+    }
+    public class Machine
+    {
+        private string m_name;
+        private float m_weight;
+        private int m_price;
+        private MachineState m_state = MachineState.Off;
+
+        public Machine(string name, float weight, int price, MachineState state)
+        {
+            this.m_name = name;
+            this.m_weight = weight;
+            this.m_price = price;
+            this.m_state = state;
+            Console.WriteLine($"새로운 {m_name}를 구매하였습니다.");
+        }
+        public string GetMachineName()
+        {
+            return m_name;
+        }
+
+        public MachineState GetState()
+        {
+            return m_state;
+        }
+
+        public void SetState(MachineState state)
+        {
+            m_state = state;
+            Console.WriteLine($"기계의 상태가 {m_state}로 변경되었습니다.");
+        }
+    }
+
+    public class Mechanic
+    {
+        private string m_name;
+
+        public Mechanic(string name)
+        {
+            m_name = name;
+        }
+        public void Repair(Machine targetMachine)
+        {
+            Console.WriteLine($"[정비중]  {m_name} 정비사가 {targetMachine.GetMachineName()}의 점검을 시작합니다.");
+            if (targetMachine.GetState() == MachineState.Broken)
+            {
+                targetMachine.SetState(MachineState.Off);
+                Console.WriteLine($"-> {targetMachine.GetMachineName()} 수리 완료! (현재 상태: Off)");
+            }
+            else
+            {
+                Console.WriteLine("기계 이상없습니다.");
+            }
+        }
+    }
     internal class Program
     {
+
+
+
+
         static void Main(string[] args)
         {
-            float a = 0f;
-            float b = 0f;
-
-            Console.WriteLine("---------------------------------------");
-            Console.WriteLine("간단한 정수 계산기 만들기!");
-            Console.WriteLine("연산 형식은 a ㅁ b 형태로 ");
-            Console.WriteLine("a의 숫자와 b 숫자를 대입한 후");
-            Console.WriteLine("사칙연산 기호를 선택하여 계산합니다");
-            Console.WriteLine("*주의* 예외처리 장치 구현이 되지않아 올바른 값을 입력하시길 바랍니다!");
-            Console.WriteLine("---------------------------------------");
-
-
-            Console.WriteLine("a 에 들어갈 수를 입력 하시오.");
-            a = float.Parse(Console.ReadLine());
-
-
-
-            Console.WriteLine("b 에 들어갈 수를 입력 하시오.");
-            b= float.Parse(Console.ReadLine());
-
-
-            Console.WriteLine("사칙 연산할 연산 기호를 입력하시오, ");
-            Console.WriteLine("+ - * / ");
-
-
-
-            while (true)
-            {
-
-                string op = Console.ReadLine();
-                
-
-                if (op == "+")
-                {
-                    Console.WriteLine(a + b);
-                }
-                else if (op == "-")
-                {
-                    Console.WriteLine(a - b);
-                }
-                else if (op == "*")
-                {
-                    Console.WriteLine(a * b);
-                }
-                else if (op == "/")
-                {
-                    Console.WriteLine(a / b);
-                }
-                else if (op == "0")
-                {
-                    break;
-                }
-
-                else
-                {
-                    Console.WriteLine("다른 기호를 입력해주세요");
-                }
-
-                Console.WriteLine("\n\n다른 사칙 연산을 하려면 다른 기호를 입력하세요.\n\n");
-                Console.WriteLine("\n\n{계산을 그만하시려면 0번을 눌러주세요}\n\n");
-            }
-
+            Machine myMouse = new Machine("고사양 게이밍 마우스", 0.6f, 300000, MachineState.Off);
+            Mechanic myMechanic = new Mechanic("차송하");
+            myMechanic.Repair(myMouse);
 
         }
     }
