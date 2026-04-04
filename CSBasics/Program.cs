9183408c7dcb39e1bde19411d26cfecde17d58a6
SonghaCH <cwithus0002@gmail.com>
2026-04-04 23:03:34 +0900

    [class] C# 클래스의 상속과 클래스 형변환을 이용한 환상 생물 사전 만들기 20260403
    클래스를 작성하며 상속의 구조와 생성자를 만드는법을 확실히 알게되고, 클래스의 형변환을 통해 일일히 다 해줘야하는 작업을 간결하게 할 수 있다는것을 알게되었다.
    기본적인 코드 구성을 통해 콘솔 창에서 보이는 부분이 깔끔해지길 바래서 AI 에게 도움을 소량 받아 완성하였다. 코드를 짜며 이넘 부분의 이해를 도왔고, 스위치문을 통해 종류가 3가지 라는 점을 생각하여 좀 더 간결하고 이해 할 수 있는 시간이 되었다.

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -1,119 +1,176 @@
 ﻿using System;
 
-namespace Toolclass
+namespace FantasticCreature
 {
-
-    public class Tool
+    public enum DangerGrade : byte
     {
-        protected string m_toolName = string.Empty;
-        protected int m_toolWeight = 0;
-        protected int m_toolDurability = 100;
-        protected string m_some = string.Empty;
-
-
-        public void callName(string name)
-        {
-            m_toolName = name;
-            Console.WriteLine($"이 도구는 {m_toolName} 입니다.");
-        }
-
-
-
-
-        public void Repair()
-        {
-            Console.WriteLine($"{m_toolName}를 수리합니다 \n");
-
-            if (m_toolDurability >= 100)
-            {
-                Console.WriteLine($"이미 내구도가 {m_toolDurability} 입니다. (100 이상일때는 수리 되지 않습니다.)\n");
-            }
-
-            else
-            {
-                m_toolDurability += 1;
-                Console.WriteLine($"{m_toolName}의 내구도가 1만큼 상승했습니다. 현재 내구도 {m_toolDurability}입니다 \n");
-            }
-
-        }
-
-        public void UseTool()
-        {
-            Console.WriteLine($"{m_toolName}를 사용합니다 \n");
-
-
-
-            if (m_toolDurability <= 0)
-            {
-                Console.WriteLine($"{m_toolName}의 내구도가 없습니다. 수리하여 주세요.\n");
-
-            }
-            else
-            {
-                m_toolDurability -= 10;
-                Console.WriteLine($"{m_toolName}의 내구도가 -10만큼 하락했습니다. 현재 내구도 {m_toolDurability} 입니다.\n");
-
-            }
-
-
-        }
-
-        public virtual void GetSomething(string some)
-        {
-            m_some = some;
-            Console.WriteLine($"{m_toolName}을 사용하였더니 {some}을 수확 하였습니다.");
-        }
-
-
+        None,
+        Low,
+        Moderate,
+        High,
+        Extreme
     }
 
-
-
-    public class Weapon : Tool
+    public class FantasticCreature
     {
+        protected string m_name = string.Empty;
+        protected int m_level = 0;
+        protected DangerGrade m_grade = DangerGrade.None;
 
-        public void callName()
+        public FantasticCreature()
         {
-
-        }
-        public override void GetSomething(string some)
-        {
-            Console.WriteLine($"{m_toolName}을 사용하였더니 {some}을 죽였습니다");
         }
 
+        public void PrintProfile()
+        {
+            Console.WriteLine($"[{m_name}]의 정보를 로드했습니다.");
+            Console.WriteLine($"이름: {m_name}");
+            Console.WriteLine($"레벨: {m_level}");
+            Console.WriteLine($"위험 등급: {m_grade}");
+            Console.WriteLine("----------------------------------");
+        }
+
+        public virtual void Attack()
+        {
+            Console.WriteLine($"{m_name}이/가 공격태세로 전환하였습니다.");
+        }
+
+        public virtual void Cry()
+        {
+            Console.WriteLine($"{m_name}이/가 숨을 들이쉽니다.");
+        }
     }
 
+    public class Dragon : FantasticCreature
+    {
+        public Dragon()
+        {
+            m_name = "드래곤";
+            m_level = 90;
+            m_grade = DangerGrade.Extreme;
+        }
 
+        public override void Attack()
+        {
+            Console.WriteLine("드래곤은 꼬리와 발톱으로 공격합니다.");
+            SpecialAttack();
+        }
 
+        public override void Cry()
+        {
+            Console.WriteLine("드래곤은 '쿠오오오오오' 라는 울음소리를 냅니다.");
+        }
 
+        public void SpecialAttack()
+        {
+            Console.WriteLine("드래곤은 '브레스'라는 특수 기술을 사용 가능합니다.");
+        }
+    }
 
+    public class Kraken : FantasticCreature
+    {
+        public Kraken()
+        {
+            m_name = "크라켄";
+            m_level = 70;
+            m_grade = DangerGrade.Moderate;
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine("크라켄은 크고 묵직한 수많은 다리로 공격합니다.");
+        }
+
+        public override void Cry()
+        {
+            Console.WriteLine("크라켄은 '쿠헬헬헬헬'이라는 울음소리를 냅니다.");
+        }
+    }
+
+    public class NineTaleFox : FantasticCreature
+    {
+        public NineTaleFox()
+        {
+            m_name = "구미호";
+            m_level = 40;
+            m_grade = DangerGrade.Moderate;
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine("구미호는 날렵한 발톱으로 공격합니다.");
+            SpecialSkill();
+        }
+
+        public override void Cry()
+        {
+            Console.WriteLine("구미호는 '아오오오오'이라는 울음소리를 냅니다.");
+        }
+
+        public void SpecialSkill()
+        {
+            Console.WriteLine("구미호는 '인간 둔갑' 이라는 특수 기술을 사용합니다.");
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Tool myTool = new Tool();
-            myTool.callName("분무기");
-            myTool.UseTool();
-            myTool.Repair();
-            myTool.GetSomething("농작물");
+            for (; ; )
+            {
+                Console.Clear();
+                Console.WriteLine("======= 환상 생물 대사전 =======");
+                Console.WriteLine("1번: 드래곤 정보");
+                Console.WriteLine("2번: 크라켄 정보");
+                Console.WriteLine("3번: 구미호 정보");
+                Console.WriteLine("4번: 사전 닫기");
+                Console.WriteLine("==================================");
+                Console.Write("어떤 생물의 데이터를 보시겠습니까? : ");
 
-            Weapon myWeapon = new Weapon();
-            myWeapon.callName("장검");
-            myWeapon.UseTool();
-            myWeapon.GetSomething("몬스터");
-            myWeapon.Repair();
+                string choice = Console.ReadLine();
+                Console.WriteLine();
 
+                if (choice == "4")
+                {
+                    Console.WriteLine("사전을 닫습니다.");
+                    break;
+                }
+
+                FantasticCreature selectedCreature = null;
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("드래곤의 정보를 선택하셨습니다.\n");
+                        selectedCreature = new Dragon();
+                        break;
+                    case "2":
+                        Console.WriteLine("크라켄의 정보를 선택하셨습니다.\n");
+                        selectedCreature = new Kraken();
+                        break;
+                    case "3":
+                        Console.WriteLine("구미호의 정보를 선택하셨습니다.\n");
+                        selectedCreature = new NineTaleFox();
+                        break;
+                    default:
+                        Console.WriteLine("올바른 번호를 선택하여 주십시오.");
+                        Console.ReadKey();
+                        continue;
+                }
+
+                if (selectedCreature != null)
+                {
+                    Console.Clear();
+
+                    selectedCreature.PrintProfile();
+                    selectedCreature.Cry();
+                    selectedCreature.Attack();
+                }
+
+                Console.WriteLine("\n아무 키나 누르면 목록으로 돌아갑니다...");
+                Console.ReadKey();
+            }
         }
     }
-
 }
-
-
-
-
-
-
-
-
-    
