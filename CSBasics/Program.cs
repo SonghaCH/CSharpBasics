b6fc11ef0162da6ff7dde92a3b6746bbee11f3ea
SonghaCH <cwithus0002@gmail.com>
2026-04-08 15:58:25 +0900

    [Function] 내부 로직 함수로 옮기기(혼자 해보기) 20260408

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -1,47 +1,46 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Xml.Linq;
+using System.Text;
 
-namespace CSharpLiveOnAir
+namespace BasicCSharp
 {
+
     internal class Program
     {
-        static void Main(string[] args)
+
+        public static void MakeCoffe(int coffeeCount)
         {
-            int coffeeCount = 10;
+            coffeeCount = 0;
             int cookedCoffeeCount = (coffeeCount * 2);
-
-            // 값복사 - 스택으로 치면 변수만 3개
-            // 매번 이렇게 받을 필요는 없다 -> 다만 변수의 흐름과 대입에 대한 이해를 위해서
-            // 의도적으로 퀴즈 문제가 그렇게 구성이 되었습니다.
-            int cafeCount = cookedCoffeeCount;
-            Console.WriteLine(cafeCount);
-
-            // 아래와 똑같음
-            // bool isCoffeeOverCooked = cookedCoffeeCount > 30;
-            // if (isCoffeeOverCooked) { }
+            int cafe = cookedCoffeeCount;
+            Console.WriteLine(cafe);
 
             if (cookedCoffeeCount > 30)
             {
+                Console.WriteLine($"요리된 커피 카운트가 {cookedCoffeeCount} 입니다.(리턴)");
                 return;
             }
 
 
-            if (cafeCount > 50)
+            if (cafe > 5)
             {
+                Console.WriteLine($"카페 수는 {cafe} 입니다.");
                 return;
             }
             else
             {
-                Console.WriteLine("카페가 정말 많네요!");
+                Console.WriteLine("정말 많네요");
             }
 
 
-            for (int i = 0; i < cafeCount; i++)
+            for (int i = 0; i < cafe; i++)
             {
-                Console.WriteLine($"카페 : {i}");
+                Console.WriteLine($"카페 {cafe}");
             }
+        }
+        public static void Main(string[] args)
+        {
+            MakeCoffe(50);
 
         }
     }
