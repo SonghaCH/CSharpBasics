e827820799218307f2c7fc962b7b18f0cff8873f
SonghaCH <cwithus0002@gmail.com>
2026-04-08 15:50:48 +0900

    [logic] 내부 로직 공부(정답) 20260408
    정답문제가 조금 잘못되어있었습니다(50보다 크게)그거 외에 로직 보강은 오케이

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -1,40 +1,46 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
+using System.Xml.Linq;
 
-namespace BasicCSharp
+namespace CSharpLiveOnAir
 {
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            int coffeeCount = 0;
+            int coffeeCount = 10;
             int cookedCoffeeCount = (coffeeCount * 2);
-            int cafe = cookedCoffeeCount;
-            Console.WriteLine(cafe);
+
+            // 값복사 - 스택으로 치면 변수만 3개
+            // 매번 이렇게 받을 필요는 없다 -> 다만 변수의 흐름과 대입에 대한 이해를 위해서
+            // 의도적으로 퀴즈 문제가 그렇게 구성이 되었습니다.
+            int cafeCount = cookedCoffeeCount;
+            Console.WriteLine(cafeCount);
+
+            // 아래와 똑같음
+            // bool isCoffeeOverCooked = cookedCoffeeCount > 30;
+            // if (isCoffeeOverCooked) { }
 
             if (cookedCoffeeCount > 30)
             {
-                Console.WriteLine($"요리된 커피 카운트가 {cookedCoffeeCount} 입니다.(리턴)");
                 return;
             }
 
 
-            if (cafe > 5)
+            if (cafeCount > 50)
             {
-                Console.WriteLine($"카페 수는 {cafe} 입니다.");
                 return;
             }
             else
             {
-                Console.WriteLine("정말 많네요");
+                Console.WriteLine("카페가 정말 많네요!");
             }
 
 
-            for (int i = 0; i < cafe; i++)
+            for (int i = 0; i < cafeCount; i++)
             {
-                Console.WriteLine($"카페 {cafe}");
+                Console.WriteLine($"카페 : {i}");
             }
 
         }
