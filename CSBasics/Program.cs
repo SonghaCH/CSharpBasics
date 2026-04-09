a0651d58c9eb420a6028f575d39918ada92b975a
SonghaCH <cwithus0002@gmail.com>
2026-04-09 23:30:37 +0900

    [List] 리스트를 활용하여 스킬 상호작용 만들기(혼자해보기 개선) 20260409
    처음에 자료구조를 담는것이 익숙하지않아서 List를 player에서 ExcuteSkill 담을 생각을 못했었는데, 거기서 담고, 메인에서 실체화 이후 그 객체 이름을 바탕으로 리스트에 있는것을 하나씩 꺼내서 쓰는 foreach 문을 사용 하는 법도 알게되었으며, 멤버 변수를 받아 이름을 바꿔주는 구조와 생각보다 . 의 활용이 중요하다는것을 알게되었다. 아직은 미숙하고 어렵지만 자료 구조 또한 차차 학습하며 점차 익숙해질것이라 생각한다.

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -6,70 +6,48 @@ using static CSBasics.Program;
 
 namespace CSBasics
 {
+    public class Player
+    {
+        public List<ExecuteSkill> skillList = new List<ExecuteSkill>();
+        public void UseSkill(string dataId)
+        {
+            Console.WriteLine(dataId);
+        }
+    }
+
+    public class ExecuteSkill
+    {
+        public string m_dataId = string.Empty;
+
+        public void UsedSkill()
+        {
+
+        }
+    }
 
     internal class Program
     {
-        public class Player
-        {
-            private List<Skill> m_mySkillList = new List<Skill>();
+        
 
 
-            public void InitAllSkill()
-            {
-                // 스킬 두개를 추가하는 역할이 Main에서 Player 스스로로 역할이전
-                var firstSkill = new Skill();
-                firstSkill.m_id = "dummySkill_01";
-
-                m_mySkillList.Add(new Skill());
-                m_mySkillList.Add(new Skill());
-                m_mySkillList.Add(new Skill());
-                m_mySkillList.Add(new Skill());
-                m_mySkillList.Add(new Skill());
-                m_mySkillList.Add(firstSkill);
-                m_mySkillList.Add(new Skill());
-            }
-
-            public void UseSkill(string skillDataId)
-            {
-                foreach (var skill in m_mySkillList)
-                {
-                    if (skill == null)
-                    { // 비교연산 - 안정적인 코드
-                        continue;
-                    }
-
-                    // m_id가 null이나 공백문자가 아니면서, && AND 스킬의 Id가 파라미터로 전달됬다
-                    bool isStringVaild = (string.IsNullOrEmpty(skill.m_id) == false);
-                    bool isSkillDataIdSame = (skill.m_id == skillDataId);
-                    bool isSkillUsable = (isStringVaild && isSkillDataIdSame);
-
-                    if (isSkillUsable == true)
-                    {
-                        skill.ExecuteSkill();
-                        break;
-                    }
-                }
-            }
-
-        }
-
-
-        public class Skill
-        {
-            public string m_id;
-
-            public void ExecuteSkill()
-            {
-                Console.WriteLine($"{m_id} 스킬을 사용함");
-            }
-        }
-
+       
 
         static void Main(string[] args)
         {
-            Player player = new Player();
-            player.InitAllSkill();
-            player.UseSkill("dummySkill_01");
+            Player player1 = new Player();
+            ExecuteSkill skill1 = new ExecuteSkill();
+            skill1.m_dataId = "파이어볼";
+            ExecuteSkill skill2 = new ExecuteSkill();
+            skill2.m_dataId = "매직애로우";
+            player1.skillList.Add(skill1);
+            
+            player1.skillList.Add(skill2);
+
+            foreach (ExecuteSkill skill in player1.skillList )
+            {
+                player1.UseSkill(skill.m_dataId);
+            }
         }
+        
     }
 }
