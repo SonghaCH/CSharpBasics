3103f0416916e6349944075841b825d16e21ec1a
SonghaCH <cwithus0002@gmail.com>
2026-04-09 15:21:21 +0900

    [List] 리스트를 활용하여 스킬 상호작용 만들기(혼자해보기) 20260409

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -4,42 +4,33 @@ using System.Text;
 
 namespace BasicCSharp
 {
+    public class Player
+    {
+        public void UseSkill(Skill dataID)
+        {
+            Console.WriteLine($"{dataID}");
+        }
+
+    }
+
+    public class Skill
+    {
+        public void UsedSkillByExecuteSkill(string dataID)
+        {
+            dataID = string.Empty;
+        }
+    }
+
 
     internal class Program
     {
-
-        public static void MakeCoffe(int coffeeCount)
-        {
-            int cookedCoffeeCount = (coffeeCount * 2);
-            int cafe = cookedCoffeeCount;
-            Console.WriteLine(cafe);
-
-            if (cookedCoffeeCount > 30)
-            {
-                Console.WriteLine($"요리된 커피 카운트가 {cookedCoffeeCount} 입니다.(리턴)");
-                return;
-            }
-
-
-            if (cafe > 5)
-            {
-                Console.WriteLine($"카페 수는 {cafe} 입니다.");
-                return;
-            }
-            else
-            {
-                Console.WriteLine("정말 많네요");
-            }
-
-
-            for (int i = 0; i < cafe; i++)
-            {
-                Console.WriteLine($"카페 {cafe}");
-            }
-        }
+        
         public static void Main(string[] args)
         {
-            MakeCoffe(50);
+           Player player1 = new Player();
+           List<Skill> skillList = new List<Skill>();
+           skillList.Add(new Skill());
+           skillList.Add(new Skill());
 
         }
     }
