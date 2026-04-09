24d1f89ea3e6c8e796f2de2507aee7f0db297edc
SonghaCH <cwithus0002@gmail.com>
2026-04-09 16:18:04 +0900

    [List] 리스트를 활용하여 스킬 상호작용 만들기(추가 문제 답안) 20260409
    - Skill이라는 클래스에 ID가 없어서 우선 추가했고
    - 스킬 초기화하는 부분(InitAllSkill())에서 특정 스킬에는 ID를 넣어 줬고 (=firstSkill)
    
    - 그리고 본 퀴즈의 목적이었던 (UseSkill에 탐색조건이 추가)
    -> 탐색조건 => 파라미터와 같은 스킬인지 판단하거라
    
    - 그외에는 코드 조금 정리

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -13,34 +13,54 @@ namespace CSBasics
         {
             private List<Skill> m_mySkillList = new List<Skill>();
 
+
+            public void InitAllSkill()
+            {
+                // 스킬 두개를 추가하는 역할이 Main에서 Player 스스로로 역할이전
+                var firstSkill = new Skill();
+                firstSkill.m_id = "dummySkill_01";
+
+                m_mySkillList.Add(new Skill());
+                m_mySkillList.Add(new Skill());
+                m_mySkillList.Add(new Skill());
+                m_mySkillList.Add(new Skill());
+                m_mySkillList.Add(new Skill());
+                m_mySkillList.Add(firstSkill);
+                m_mySkillList.Add(new Skill());
+            }
+
             public void UseSkill(string skillDataId)
             {
                 foreach (var skill in m_mySkillList)
                 {
                     if (skill == null)
-                    {
+                    { // 비교연산 - 안정적인 코드
                         continue;
                     }
-                    skill.ExecuteSkill();
+
+                    // m_id가 null이나 공백문자가 아니면서, && AND 스킬의 Id가 파라미터로 전달됬다
+                    bool isStringVaild = (string.IsNullOrEmpty(skill.m_id) == false);
+                    bool isSkillDataIdSame = (skill.m_id == skillDataId);
+                    bool isSkillUsable = (isStringVaild && isSkillDataIdSame);
+
+                    if (isSkillUsable == true)
+                    {
+                        skill.ExecuteSkill();
+                        break;
+                    }
                 }
-
-                Console.WriteLine(skillDataId);
             }
 
-            public void InitAllSkill()
-            {
-                m_mySkillList.Add(new Skill());
-                m_mySkillList.Add(new Skill());
-            }
         }
 
 
         public class Skill
         {
+            public string m_id;
 
             public void ExecuteSkill()
             {
-                Console.WriteLine("스킬을 사용함");
+                Console.WriteLine($"{m_id} 스킬을 사용함");
             }
         }
 
@@ -52,5 +72,4 @@ namespace CSBasics
             player.UseSkill("dummySkill_01");
         }
     }
-}
 }
