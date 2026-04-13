bdf60e91bf0776267ef687d5be18013392d9335b
SonghaCH <cwithus0002@gmail.com>
2026-04-13 17:16:00 +0900

    [Interface] 인터페이스의 사용으로 하루 일과 만들기 20260413
    인터페이스의 기능을 학습하며, 사용하는 법과 구조를 알게되었다.
    상속의 구조이지만 이는 다중 상속이 불가능한 C# 에서의 다중 상속의 형태로 만들어주는 역할을 한다. 간단한 클래스 생성으로 사용하는법을 확실히 알게되었다.

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -6,85 +6,119 @@ using static CSBasics.Program;
 
 namespace CSBasics
 {
-    public class Furniture
+    public interface IStudyClass
     {
-        public int m_furnitureID;
-        public string m_furnitureName;
-        public string m_furnitureDescription;
+        void TakeClass();
+        void DoHomework();
+    }
+    
+    public interface IHaveLunch
+    {
+        void HaveLunch();
+    }
 
-        public void InitFurnitureInfo()
+    public interface ITakeRest
+    {
+        void TakeRest();
+    }
+
+
+
+
+
+    public class Songha : IStudyClass , IHaveLunch, ITakeRest
+    {
+        public void Work()
         {
-            m_furnitureID = 0;
-            m_furnitureName = string.Empty;
-            m_furnitureDescription = string.Empty;
+            Console.WriteLine("나는 편의점 알바를 한다.");
+        }
+
+        public void TakeClass()
+        {
+            Console.WriteLine("수업을 들으며, 앞으로를 꿈꿉니다.");
+        }
+
+        public void DoHomework()
+        {
+            Console.WriteLine("Ai의 코드를 보지 않으려 노력하며 과제를 합니다.");
+        }
+
+        public void Game()
+        {
+            Console.WriteLine($"나는 게임을 하기로 했습니다. 어떤 게임을 할건지 입력하시오.");
+            Console.WriteLine($"<진행하시려면 게임 입력후 6기 멤버들의 하루를 탐색하시오.>");
+
+            string gameName = Console.ReadLine();
+
+            Console.WriteLine($"나는 {gameName} 게임을 하였다.\n\n");
+
 
         }
+
+        public void HaveLunch()
+        {
+            Console.WriteLine("난 오늘 점심으로 콩나물 국을 먹었다. ");
+        }
+
+        public void TakeRest()
+        {
+            Console.WriteLine("쉬는 시간마다 누워서 안정을 취합니다.");
+        }
+    }
+    
+
+    public class Gm06Members : IHaveLunch,ITakeRest,IStudyClass
+    {
+        public void GodLife()
+        {
+            Console.WriteLine("6기 멤버들은 수업 이후에도 갓생을 산다 ");
+        }
+
+        public void TakeClass()
+        {
+            Console.WriteLine("6기 멤버들은 강사님에게 경청하며 스킬들을 체화한다 .");
+        }
+
+        public void DoHomework()
+        {
+            Console.WriteLine("멤버들은 엄청난 기량으로 과제를 해치운다.");
+        }
+        public void HaveLunch()
+        {
+            Console.WriteLine("6기 멤버들은 호화로운 점심을 먹는다. ");
+        }
+
+        public void TakeRest()
+        {
+            Console.WriteLine("쉬는 시간에도 열심히 공부한다.");
+        }
+
 
     }
 
-    public class FurnitureStore
-    {
-        public Dictionary<string, Furniture> m_todayFurniture = new Dictionary<string, Furniture>();
-        public void bringTodayFurniture()
-        {
-            Furniture furniture = new Furniture();
-            furniture.InitFurnitureInfo();
-
-
-            List<string> makeFurnitureListName = new List<string>();
-            makeFurnitureListName.Add("냉장고");
-            makeFurnitureListName.Add("TV");
-            makeFurnitureListName.Add("스타일러");
-            makeFurnitureListName.Add("라디오");
-            makeFurnitureListName.Add("컴퓨터");
-            makeFurnitureListName.Add("피아노");
-            makeFurnitureListName.Add("선풍기");
-            makeFurnitureListName.Add("에어컨");
-            makeFurnitureListName.Add("세탁기");
-            makeFurnitureListName.Add("의자");
-
-            for (int i = 0; i < makeFurnitureListName.Count; i++)
-            {
-                Furniture addFurniture = new Furniture();
-                addFurniture.InitFurnitureInfo();
-
-                addFurniture.m_furnitureName = makeFurnitureListName[i];
-                addFurniture.m_furnitureID = i;
-
-                m_todayFurniture.Add(makeFurnitureListName[i], addFurniture);
-
-            }
-
-
-
-
-        }
-
-        public void openStore()
-        {
-            foreach (Furniture OpenItem in m_todayFurniture.Values)
-            {
-                Console.WriteLine($"가구 Id : {OpenItem.m_furnitureID}, 가구 이름 : {OpenItem.m_furnitureName}");
-            }
-        }
-    }
 
 
 
     internal class Program
     {
 
-
-
-
-
         static void Main(string[] args)
         {
-            Furniture furniture1 = new Furniture();
-            FurnitureStore bringFurniture = new FurnitureStore();
-            bringFurniture.bringTodayFurniture();
-            bringFurniture.openStore();
+           Songha songha = new Songha();
+            songha.TakeClass();
+            songha.TakeRest();
+            songha.HaveLunch();
+            songha.DoHomework();
+            songha.Work();
+            songha.Game();
 
+            Gm06Members friend1 = new Gm06Members();
+            friend1.TakeClass();
+            friend1.TakeRest();
+            friend1.HaveLunch();
+            friend1.DoHomework();
+            friend1.DoHomework();
+            friend1.GodLife();
         }
 
     }
