855457162e3861800aad2759cd12fa68553d0dea
SonghaCH <cwithus0002@gmail.com>
2026-04-14 16:53:14 +0900

    [Property] get set의 활용으로 멤버변수 수정 및 호출 해보기 20260414
    프로퍼티의 중요성과 get set 의 변천사를 학습하며, 발전되는 형태를 공부하고 멤버 변수를 여러가지 형태로 수정 및 호출을 할 수 있는 능력을 학습하였다.
    쓰지않던 문법이라 익숙해질 필요가 있다.

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -6,119 +6,61 @@ using static CSBasics.Program;
 
 namespace CSBasics
 {
-    public interface IStudyClass
+    public class Player
     {
-        void TakeClass();
-        void DoHomework();
+        private string m_name;
+        private int m_damage;
+        public int m_hp { get; private set; }
+
+        public Player()
+        { 
+            m_name = string.Empty;
+            m_damage = 0;
+            m_hp = 100;
+        }
+        
+        public string GetName()
+        {
+            return m_name;
+        }
+
+        public void SetName(string name)
+        {
+           m_name = name;
+        }
+
+        public int Damage
+        {
+            get { return m_damage; }
+            set {
+                if(m_damage != value)
+                    m_damage = value;
+                }
+        }
+
+        public void ChangeHp(int hp)
+        {
+            m_hp = hp;
+        }
+        
     }
-    
-    public interface IHaveLunch
-    {
-        void HaveLunch();
-    }
-
-    public interface ITakeRest
-    {
-        void TakeRest();
-    }
-
-
-
-
-
-    public class Songha : IStudyClass , IHaveLunch, ITakeRest
-    {
-        public void Work()
-        {
-            Console.WriteLine("나는 편의점 알바를 한다.");
-        }
-
-        public void TakeClass()
-        {
-            Console.WriteLine("수업을 들으며, 앞으로를 꿈꿉니다.");
-        }
-
-        public void DoHomework()
-        {
-            Console.WriteLine("Ai의 코드를 보지 않으려 노력하며 과제를 합니다.");
-        }
-
-        public void Game()
-        {
-            Console.WriteLine($"나는 게임을 하기로 했습니다. 어떤 게임을 할건지 입력하시오.");
-            Console.WriteLine($"<진행하시려면 게임 입력후 6기 멤버들의 하루를 탐색하시오.>");
-
-            string gameName = Console.ReadLine();
-
-            Console.WriteLine($"나는 {gameName} 게임을 하였다.\n\n");
-
-
-        }
-
-        public void HaveLunch()
-        {
-            Console.WriteLine("난 오늘 점심으로 콩나물 국을 먹었다. ");
-        }
-
-        public void TakeRest()
-        {
-            Console.WriteLine("쉬는 시간마다 누워서 안정을 취합니다.");
-        }
-    }
-    
-
-    public class Gm06Members : IHaveLunch,ITakeRest,IStudyClass
-    {
-        public void GodLife()
-        {
-            Console.WriteLine("6기 멤버들은 수업 이후에도 갓생을 산다 ");
-        }
-
-        public void TakeClass()
-        {
-            Console.WriteLine("6기 멤버들은 강사님에게 경청하며 스킬들을 체화한다 .");
-        }
-
-        public void DoHomework()
-        {
-            Console.WriteLine("멤버들은 엄청난 기량으로 과제를 해치운다.");
-        }
-        public void HaveLunch()
-        {
-            Console.WriteLine("6기 멤버들은 호화로운 점심을 먹는다. ");
-        }
-
-        public void TakeRest()
-        {
-            Console.WriteLine("쉬는 시간에도 열심히 공부한다.");
-        }
-
-
-    }
-
-
-
 
     internal class Program
     {
 
         static void Main(string[] args)
         {
-           Songha songha = new Songha();
-            songha.TakeClass();
-            songha.TakeRest();
-            songha.HaveLunch();
-            songha.DoHomework();
-            songha.Work();
-            songha.Game();
+           Player player1 = new Player();
+            
+            
+            player1.SetName("철수");
+            Console.WriteLine(player1.GetName());
 
-            Gm06Members friend1 = new Gm06Members();
-            friend1.TakeClass();
-            friend1.TakeRest();
-            friend1.HaveLunch();
-            friend1.DoHomework();
-            friend1.DoHomework();
-            friend1.GodLife();
+            player1.Damage = 10;
+            Console.WriteLine(player1.Damage);
+
+            player1.ChangeHp(10000000);
+            Console.WriteLine(player1.m_hp);
         }
 
     }
