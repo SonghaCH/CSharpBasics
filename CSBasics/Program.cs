c6120ff4bb13ac4eb7a04964034af6c340c47f9a
SonghaCH <cwithus0002@gmail.com>
2026-04-14 17:39:08 +0900

    [Property] get set 을 이용한 클래스간의 상호작용 해보기 20260414
    이전 get set의 활용을 통해서 클래스간의 상호작용 할 때 접근 제한자의 중요성과
    get set 의 유용함을 학습하고, 변수의 흐름과  실체화된 객체의 상호작용을 이해하고 단순한 배틀을 구현하였다. 변수의 흐름을 파악하는것이 핵심이며, get set 을 체화 하는것이 앞으로의 목적이다.

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -6,61 +6,113 @@ using static CSBasics.Program;
 
 namespace CSBasics
 {
+
     public class Player
     {
-        private string m_name;
-        private int m_damage;
+       
+
+        public string m_name { get; private set; }
+        public int m_atk { get; private set; }
         public int m_hp { get; private set; }
 
-        public Player()
-        { 
-            m_name = string.Empty;
-            m_damage = 0;
-            m_hp = 100;
-        }
-        
-        public string GetName()
+        public Player(string name,int attack,int hp)
         {
-            return m_name;
-        }
-
-        public void SetName(string name)
-        {
-           m_name = name;
-        }
-
-        public int Damage
-        {
-            get { return m_damage; }
-            set {
-                if(m_damage != value)
-                    m_damage = value;
-                }
-        }
-
-        public void ChangeHp(int hp)
-        {
+            m_name = name;
+            m_atk = attack;
             m_hp = hp;
         }
-        
+
+        public void ChangeName(string playerName)
+        {
+            m_name = playerName;
+        }
+
+
+        public void ChangeAtk(int playerAtk)
+        {
+            m_atk = playerAtk;
+        }
+        public void ChangeHp(int playerCurentHp)
+        {
+            m_hp = playerCurentHp;
+        }
+
+        public void Attack(Monster targetMonster)
+        {
+            Console.WriteLine($"{m_name} 이/가  {targetMonster.m_name} 을/를 공격했다.~");
+            Console.WriteLine($"{m_atk}만큼의 피해를 주었다.");
+            targetMonster.TakeDamage(m_atk);
+
+        }
+        public void TakeDamage(int monsterDamage)
+        {
+            m_hp = m_hp - monsterDamage;
+            Console.WriteLine($"{m_name}의 체력이 {m_hp}가 되었다.~");
+
+        }
+
+
     }
+
+    public class Monster
+    {
+        public string m_name { get; private set; }
+        public int m_atk { get; private set; }
+        public int m_hp { get; private set; }
+
+        public Monster(string name, int attack, int hp)
+        {
+            m_name = name;
+            m_atk = attack;
+            m_hp = hp;
+        }
+
+        public void ChangeName(string monsterName)
+        {
+            m_name = monsterName;
+        }
+
+        public void ChangeAtk(int monsterAtk)
+        {
+            m_atk = monsterAtk;
+        }
+        public void ChangeHp(int monseterCurentHp)
+        {
+            m_hp = monseterCurentHp;
+        }
+        
+
+        public void Attack(Player targetPlayer)
+        {
+            Console.WriteLine($"{m_name} 이/가  {targetPlayer.m_name} 을/를 공격했다.~");
+            Console.WriteLine($"{m_atk}만큼의 피해를 주었다.");
+            targetPlayer.TakeDamage(m_atk);
+
+
+        }
+        public void TakeDamage(int playerDamage)
+        {
+            m_hp = m_hp - playerDamage; 
+            Console.WriteLine($"{m_name}의 체력이 {m_hp}가 되었다.~");
+        }
+
+    }
+
+
+
+
 
     internal class Program
     {
 
         static void Main(string[] args)
         {
-           Player player1 = new Player();
-            
-            
-            player1.SetName("철수");
-            Console.WriteLine(player1.GetName());
+            Player player1 = new Player("잠만보",50,1000);
+            Monster monster1 = new Monster("리자몽",100,500);
+            player1.Attack(monster1);
+            monster1.Attack(player1);
+           
 
-            player1.Damage = 10;
-            Console.WriteLine(player1.Damage);
-
-            player1.ChangeHp(10000000);
-            Console.WriteLine(player1.m_hp);
         }
 
     }
